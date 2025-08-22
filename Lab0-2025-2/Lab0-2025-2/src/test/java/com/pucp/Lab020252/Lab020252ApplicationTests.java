227dbe6b2e29e20344d1252aa758033e93b54e51
david <a20224848@gmail.com>
2025-08-21 20:25:57 -0500

    hola mundo

---
+++ Lab0-2025-2/Lab0-2025-2/src/test/java/com/pucp/Lab020252/Lab020252ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucp.Lab020252;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab020252ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
